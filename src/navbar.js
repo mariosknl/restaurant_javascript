fafd16e28f69a7376319f22206e103f47b996183
mariosknl <mariosknl@outlook.com>
2020-07-01 19:24:16 +0300

    Container && Navbar created by JS

---
+++ src/navbar.js
@@ -0,0 +1,19 @@
+const navbar = `  <div class="container mt-3 shadow-lg content">
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a href="#" class="navbar-brand font-weight-bold nav-link" id="homeButton">
+          Greek Restaurant
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link text-black font-weight-bold" id="bookButton">BOOK A TABLE</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link text-black font-weight-bold" id="menuButton">MENU</a>
+      </li>
+    </ul>
+  </nav>
+</div>`;
+
+export default { navbar };
