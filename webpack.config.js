d3a3cc13a5c98092f1d9e637f5b6e5c188b709c2
mariosknl <mariosknl@outlook.com>
2020-07-03 17:28:54 +0300

    ContentHash && HtmlWebpackPlugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main.[contentHash].js',
     path: path.join(__dirname, 'dist'),
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/template.html',
+  })],
   module: {
     rules: [
       {
