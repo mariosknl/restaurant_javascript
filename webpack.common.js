f7f3a9383860419dbd00f1aff8238e8b6b018124
mariosknl <mariosknl@outlook.com>
2020-07-05 18:06:19 +0300

    Changed made in the webpack.config file

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,10 @@
 const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/template.html',
+  })],
 };
